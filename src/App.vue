d525fca412a5d49d997b81dfccf58ffd54a9ab53
gudetama <daniellewwise@gmail.com>
2021-03-21 22:57:07 -0400

    removed hello world component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <BaseTimer />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BaseTimer from './components/BaseTimer'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BaseTimer
   }
 }
 </script>
