2de72b64ba50a2785896233dec45e09b59cfb60c
gudetama <daniellewwise@gmail.com>
2021-03-26 13:16:10 -0400

    moved properties into BaseTimer

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,14 @@
 <template>
-  <BaseTimer :time-left="timeLeft" />
+  <BaseTimer />
 </template>
 
 <script>
 import BaseTimer from './components/BaseTimer'
-const TIME_LIMIT = 20;
+
 export default {
   name: 'App',
   components: {
     BaseTimer
-  },
-  data() {
-    return {
-      timePassed: 0,
-      timerInterval: null
-    }
-  },
-  methods: {
-      startTimer() {
-      this.timerInterval = setInterval(() => (this.timePassed += 1), 1000);
-    }
-  },
-  mounted() {
-    this.startTimer();
-    console.log('timer mounted!')
-  },
-  computed: {
-    timeLeft() {
-      return TIME_LIMIT - this.timePassed;
-    }
   }
 }
 </script>
